01584d9af212c5bbf8e41b07b2c997b4b2357b3e
devpatel802 <devrpatel0201@gmail.com>
2024-08-31 13:55:37 +0530

    dona

--- db_connect.php
+++ db_connect.php
@@ -11,3 +11,5 @@ if ($conn->connect_error) {
 }
 echo "";
 ?>
+
+
--- event.php
+++ event.php
@@ -71,7 +71,7 @@ require 'db_connect.php';
             <div class="container">
                 <nav class="navbar navbar-light navbar-expand-lg py-4">
                     <a href="index.html" class="navbar-brand">
-                        <h1 class="text-primary fw-bold mb-0">Parampara<span class="text-dark">Exhibition</span> </h1>
+                    <h1 class="text-primary fw-bold mb-0"><span class="text-dark"><img src="img/logo.png" class="img-fluid rounded animated zoomIn" style="width:127px" alt=""></span> </h1>
                     </a>
                     <button class="navbar-toggler py-2 px-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                         <span class="fa fa-bars text-primary"></span>
@@ -88,7 +88,6 @@ require 'db_connect.php';
                                 <div class="dropdown-menu bg-light">
                                     <a href="book.html" class="dropdown-item">Booking</a>
                                     <a href="blog.html" class="dropdown-item">Our Blog</a>
-                                    <a href="team.html" class="dropdown-item">Our Team</a>
                                     <a href="testimonial.html" class="dropdown-item">Testimonial</a>
                                     <!-- <a href="404.html" class="dropdown-item">404 Page</a> -->
                                 </div>
