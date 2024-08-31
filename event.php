aae74ad15491eb057b8353c832fa7905fc62eee5
devpatel802 <devrpatel0201@gmail.com>
2024-08-31 14:47:23 +0530

    final

--- event.php
+++ event.php
@@ -71,7 +71,12 @@ require 'db_connect.php';
             <div class="container">
                 <nav class="navbar navbar-light navbar-expand-lg py-4">
                     <a href="index.html" class="navbar-brand">
-                    <h1 class="text-primary fw-bold mb-0"><span class="text-dark"><img src="img/logo.png" class="img-fluid rounded animated zoomIn" style="width:127px" alt=""></span> </h1>
+                    <div class="text-center">
+                            <img src="img/logo.png" class="img-fluid rounded animated zoomIn mb-2" style="width:127px;" alt="">
+                            <h1 class="text-primary fw-bold mb-0">
+                                Parampara<span class="text-dark">Exhibition</span>
+                            </h1>
+                        </div>  
                     </a>
                     <button class="navbar-toggler py-2 px-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                         <span class="fa fa-bars text-primary"></span>
